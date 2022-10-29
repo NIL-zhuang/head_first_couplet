27e662d916f3df12904977c50f281fd5bbc69be8
zhuang-ziyuan <zhuang-ziyuan@qq.com>
2022-10-29 16:04:37 +0800

    couplet

--- src/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-WORKING_DIR = os.path.abspath(
-    os.path.join(
-        os.path.abspath(__file__),
-        "../..")
-)
-COUPLET_PATH = os.path.join(WORKING_DIR, "data", "couplet")
--- src/data/dataset_download.py
+++
@@ -1,3 +0,0 @@
-# https://github.com/chinese-poetry/chinese-poetry
-# GitHub - chinese-poetry/chinese-poetry: The most comprehensive database of Chinese poetry
-# 🧶最全中华古诗词数据库, 唐宋两朝近一万四千古诗人, 接近5.5万首唐诗加26万宋诗
